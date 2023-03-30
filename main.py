21846e2b5a9803c96e3d1b6f003ad0fa4fd8b04e
HappyMatDuckling <matveyd05@yandex.ru>
2023-03-31 00:24:13 +0300

    second commit

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from flask import Flask, flash, request, redirect, url_for, make_response, rende
 from flask import send_from_directory, send_file
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/home/HappyMatDuckling/mysite/data'
+UPLOAD_FOLDER = ''  # '/home/HappyMatDuckling/mysite/data'
 ALLOWED_EXTENSIONS = {'txt', 'docx', 'doc'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app.secret_key = 'super secret key'
 
 
 def allowed_file(filename):
@@ -18,26 +20,35 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == "POST":
-        if 'file' not in request.files:
-            flash('Файл не читается')
-            return redirect(request.url)
-        file = request.files['file']
+        file = request.files['input_file']
         if not file.filename:
             flash('Нет выбранного файла')
             return redirect("/")
         if file and allowed_file(file.filename):
-            my_file = request.files["input_file"]
-            data = my_file.stream.read().decode("utf-8")
-            with open(f"{app.config['UPLOAD_FOLDER']}/data/bad_sites.txt", "at", encoding="utf8") as block_sites:
-                block_sites.write(data)
+            data = file.stream.read().decode("utf-8")
+            with open(f"{app.config['UPLOAD_FOLDER']}data/bad_sites.txt", "at", encoding="utf8") as block_sites:
+                for el in data.split():
+                    block_sites.write(el + "\n")
             return redirect("/")
 
-    return  render_template('base.html')
+    return render_template('main_page.html')
+
+
+@app.route('/list')
+def list_sites():
+    with open(f"{app.config['UPLOAD_FOLDER']}data/bad_sites.txt", "rt", encoding="utf8") as block_sites:
+        params = block_sites.readlines()
+    return render_template('site_list.html', params=params)
 
 
 @app.route('/uploads/<name>')
 def download_file(name):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], "bad_sites.txt")
+    return send_from_directory(app.config['UPLOAD_FOLDER'], "data/bad_sites.txt")
+
+
+@app.route('/block')
+def block_sites():
+    return render_template('block.html')
 
 
 if __name__ == '__main__':
