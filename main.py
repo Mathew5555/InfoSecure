896c422e0492e715523ff94a8bf3801e0781177e
HappyMatDuckling <matveyd05@yandex.ru>
2023-04-11 22:16:16 +0300

    ** New Big Update ** 1) add new sites to blocked list from file (*.docx and *.txt) or with text message 2) new design

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import os
 from flask import Flask, flash, request, redirect, url_for, make_response, render_template
 from flask import send_from_directory, send_file
 from werkzeug.utils import secure_filename
+from docx import Document
 
-UPLOAD_FOLDER = ''  # '/home/HappyMatDuckling/mysite/data'
+UPLOAD_FOLDER = ''  # '/home/HappyMatDuckling/mysite/'
 ALLOWED_EXTENSIONS = {'txt', 'docx', 'doc'}
 
 app = Flask(__name__)
@@ -20,16 +21,34 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == "POST":
-        file = request.files['input_file']
-        if not file.filename:
-            flash('Нет выбранного файла')
-            return redirect("/")
-        if file and allowed_file(file.filename):
-            data = file.stream.read().decode("utf-8")
+        if "submit-2" in request.values:
+            text = request.values["message"].split("\r\n")
+            with open(f"{app.config['UPLOAD_FOLDER']}data/bad_sites.txt", "rt", encoding="utf8") as block_sites:
+                lines = list(map(str.strip, block_sites.readlines()))
             with open(f"{app.config['UPLOAD_FOLDER']}data/bad_sites.txt", "at", encoding="utf8") as block_sites:
-                for el in data.split():
-                    block_sites.write(el + "\n")
-            return redirect("/")
+                for el in text:
+                    if el not in lines:
+                        block_sites.write(el + "\n")
+        else:
+            file = request.files['input_file']
+            if not file.filename:
+                flash('Нет выбранного файла')
+                return redirect("/")
+            if file and allowed_file(file.filename):
+                if file.filename.split('.')[-1].lower() == "txt":
+                    data = file.stream.read().decode("utf-8").split()
+                else:
+                    document = Document(file.filename)
+                    data = []
+                    for par in document.paragraphs:
+                        data.append(par.text)
+                with open(f"{app.config['UPLOAD_FOLDER']}data/bad_sites.txt", "rt", encoding="utf8") as block_sites:
+                    lines = list(map(str.strip, block_sites.readlines()))
+                with open(f"{app.config['UPLOAD_FOLDER']}data/bad_sites.txt", "at", encoding="utf8") as block_sites:
+                    for el in data:
+                        if el not in lines:
+                            block_sites.write(el + "\n")
+                return redirect("/")
 
     return render_template('main_page.html')
 
